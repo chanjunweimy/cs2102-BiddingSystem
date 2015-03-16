ccbbee173d066859320906ec6d6b2a7c9b066d63
a0111770 <tanteckli@gmail.com>
2015-03-16 19:21:29 +0800

    new changes

---
+++ mywebsite/studentTimeTable.php
@@ -0,0 +1,63 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/Home2.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Module Bidding</title>
+<!-- InstanceEndEditable -->
+<link href="portalJ.css" rel="stylesheet" type="text/css" />
+<script src="../	Scripts/swfobject_modified.js" type="text/javascript"></script>
+<script type="text/javascript" src="chrome.js">
+</script>
+<!-- InstanceBeginEditable name="head" -->
+<script src="../SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+<link href="../SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css" />
+<!-- InstanceEndEditable -->
+</head>
+
+<body>
+
+<div class="container">
+
+<div class="logo">
+<a href="index.php"><img src="images/logo.JPG" alt="Insert Logo Here" name="Insert_logo" width="318" height="160" id="Insert_logo" style="background: #FFF; display:block;" /></a> 
+</div>
+
+<div class="navbar">  
+
+<div class="chromestyle" id="chromemenu">
+<ul>
+<li><a href="studentTimeTable.php">Home</a></li>
+<li><a href="logout.php">Logout</a></li>
+</ul>
+</div>
+</div>
+<!-- InstanceBeginEditable name="banner" -->
+<div class="header">
+  <!--<h1><font color="#FFFFFF"> header</font></h1>-->  <!-- end .header -->
+</div>
+<!-- InstanceEndEditable --><!-- InstanceBeginEditable name="content" -->
+<div class="content">
+  <font size="-1">
+  
+  <h1><br>Time Table</br></h1>
+  
+</font>	
+  <!-- end .content -->
+  </div>
+<!-- InstanceEndEditable -->
+<div class="footer">
+<center><font size="-2">
+    <p>Database ~~ Copyright 2015 ~~ Wallpaper & Logo by Jamal Azizi@ jmalandme.clanteam.com ~~ All Rights Reserved.</p>
+</font>
+</center>
+    <!-- end .footer --></div>
+  <!-- end .container --></div>
+<!-- InstanceBeginEditable name="flash" -->
+<script type="text/javascript">
+var sprytextfield1 = new Spry.Widget.ValidationTextField("sprytextfield1");
+var sprytextfield2 = new Spry.Widget.ValidationTextField("sprytextfield2");
+</script>
+<!-- InstanceEndEditable -->
+</body>
+<!-- InstanceEnd --></html>
