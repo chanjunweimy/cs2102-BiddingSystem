2918e60e8ed21507985f66ed81304fe7d2dbb543
Ke You <cheongkeyou@gmail.com>
2015-03-22 02:35:47 +0800

    added preclusion and prerequisite checks to module selection.

--- mywebsite/studentSelectionForm.php
+++ mywebsite/studentSelectionForm.php
@@ -48,7 +48,48 @@ if(isset($_POST['selectModule']))
 		oci_execute($stid);	
 		$isTimeClashed = oci_fetch_array($stid, OCI_RETURN_NULLS)[0];
 
-		if (!$isModuleSelected && !$isTimeClashed) {
+		$stid = oci_parse($conn, "SELECT 'True'
+								  FROM preclusion p, passed ps
+								  WHERE p.excludedmodule = ps.moduleCode
+								  AND ps.matricNo = '$matric'
+								  AND p.module = '$moduleCode'");
+
+		oci_execute($stid);	
+		$isPrecluded = oci_fetch_array($stid, OCI_RETURN_NULLS)[0];
+		
+		$stid = oci_parse($conn, "SELECT 'True'
+								  FROM prerequisite p
+								  WHERE p.module = '$moduleCode'");
+
+		oci_execute($stid);	
+		$isModulesRequired = oci_fetch_array($stid, OCI_RETURN_NULLS)[0];
+		
+		$stid = oci_parse($conn, "SELECT 'True'
+								  FROM prerequisite p
+								  WHERE EXISTS(
+									SELECT p2.andId
+									FROM prerequisite p2
+									WHERE p2.module = p.module
+									AND p2.andId = p.andId
+									AND p2.requiredModule IN (
+										SELECT ps.moduleCode
+										FROM passed ps
+										WHERE ps.matricNo = '$matric'
+									)
+									GROUP BY p2.andId
+									HAVING COUNT(p2.requiredModule) = (
+										SELECT COUNT(p3.requiredModule)
+										FROM prerequisite p3
+										WHERE p3.module = p.module
+										AND p3.andId = p.andId
+									)
+								  )
+								  AND p.module = '$moduleCode'");
+
+		oci_execute($stid);	
+		$isRequirementMet = oci_fetch_array($stid, OCI_RETURN_NULLS)[0];
+
+		if (!$isModuleSelected && !$isTimeClashed && !$isPrecluded && (!$isModulesRequired || $isRequirementMet)) {
 			$stid = oci_parse($conn,"INSERT INTO selected (matricNo, moduleCode, startTime, endTime, day, bidpoints, bidTime)
 								     values('$matric', '$moduleCode', '$startTime', '$endTime', '$day', '$bidpoints', CURRENT_TIMESTAMP) ");
 			oci_execute($stid);	
